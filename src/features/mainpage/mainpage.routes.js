59301dc786d390058a2878107cf3912234b217c5
BasiliscX <juniornavarro1988@gmail.com>
2024-06-26 17:13:38 -0300

    added 'welcome' page.

--- src/features/mainpage/mainpage.routes.js
+++ src/features/mainpage/mainpage.routes.js
@@ -1,9 +1,14 @@
+// src/features/mainpage/mainpage.routes.js
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('mainpage');
+    const filePath = path.join(__dirname, '../views/welcome.html');
+    res.sendFile(filePath);
 });
 
 export default router;
